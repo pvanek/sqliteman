6b77ecb8c2939c7e148714c71db3443d997c156a
pvanek <petr@yarpen.cz>
2007-08-30 13:36:17 +0000

    import preview works for CSV

--- Sqliteman/sqliteman/importtabledialog.cpp
+++ Sqliteman/sqliteman/importtabledialog.cpp
@@ -5,12 +5,14 @@ a copyright and/or license notice that predates the release of Sqliteman
 for which a new license (GPL+exception) is in place.
 */
 #include <QFileDialog>
+#include <QMessageBox>
 
 #include "importtabledialog.h"
 #include "database.h"
 #include "sqliteprocess.h"
 
 #include <QtDebug>
+
 ImportTableDialog::ImportTableDialog(QWidget * parent, const QString & tableName, const QString & schema)
 	: QDialog(parent),
 	  m_parent(parent)
@@ -31,6 +33,14 @@ ImportTableDialog::ImportTableDialog(QWidget * parent, const QString & tableName
 
 	connect(fileButton, SIGNAL(clicked()), this, SLOT(fileButton_clicked()));
 	connect(buttonBox, SIGNAL(accepted()), this, SLOT(slotAccepted()));
+	connect(tabWidget, SIGNAL(currentChanged(int)), this, SLOT(createPreview(int)));
+	// csv
+	connect(pipeRadioButton, SIGNAL(clicked(bool)), this, SLOT(createPreview(bool)));
+	connect(commaRadioButton, SIGNAL(clicked(bool)), this, SLOT(createPreview(bool)));
+	connect(semicolonRadioButton, SIGNAL(clicked(bool)), this, SLOT(createPreview(bool)));
+	connect(tabelatorRadioButton, SIGNAL(clicked(bool)), this, SLOT(createPreview(bool)));
+	connect(customRadioButton, SIGNAL(clicked(bool)), this, SLOT(createPreview(bool)));
+	connect(customEdit, SIGNAL(textChanged(QString)), this, SLOT(customEdit_textChanged(QString)));
 }
 
 void ImportTableDialog::fileButton_clicked()
@@ -39,47 +49,142 @@ void ImportTableDialog::fileButton_clicked()
 	pth = pth.isEmpty() ? QDir::currentPath() : pth;
 	QString fname = QFileDialog::getOpenFileName(this, tr("File to Import"),
 												 pth,
-												 tr("Text Files (*.txt);;All Files (*)"));
+												 tr("CSV Files (*.csv);;Text Files (*.txt);;All Files (*)"));
 	if (fname.isEmpty())
 		return;
 
 	fileEdit->setText(fname);
+	createPreview();
 }
 
 void ImportTableDialog::slotAccepted()
 {
 	bool result = false;
+
 	switch (tabWidget->currentIndex())
 	{
-		case 0 :
+		case 0:
 			result = sqliteImport();
 			break;
-		// TODO: more stuff!
+		// TODO: more stuff
 	}
-
 	if (result)
 		accept();
 }
 
+QString ImportTableDialog::sqliteSeparator()
+{
+	if (pipeRadioButton->isChecked())
+		return "|";
+	else if (commaRadioButton->isChecked())
+		return ",";
+	else if (semicolonRadioButton->isChecked())
+		return ";";
+	else if (tabelatorRadioButton->isChecked())
+		return "\"\\t\"";
+	return customEdit->text();
+}
+
 bool ImportTableDialog::sqliteImport()
 {
-	QString out, sep;
+	QString out;
+	if (sqliteSeparator().length() == 0)
+	{
+		QMessageBox::warning(this, tr("Data Import"),
+							 tr("Fields separator must be given"));
+		return false;
+	}
 	SqliteProcess imp(m_parent);
-
-	if (pipeRadioButton->isChecked())
-		sep = "|";
-	else if (commaRadioButton->isChecked())
-		sep = ",";
-	else if (semicolonRadioButton->isChecked())
-		sep = ";";
-	else if (tabelatorRadioButton->isChecked())
-		sep = "\"\\t\"";
-	else if (customRadioButton->isChecked())
-		sep = customEdit->text();
-
-	imp.start(QStringList() << ".separator " << sep << " .import " << fileEdit->text() << " " << tableComboBox->currentText());
+	imp.start(QStringList() << ".separator " << sqliteSeparator() << " .import " << fileEdit->text() << " " << tableComboBox->currentText());
 	qDebug() << imp.errorMessage();
 	qDebug() << imp.success();
 	qDebug() << imp.allStderr();
 	return false;
+}
+
+void ImportTableDialog::createPreview(int)
+{
+	switch (tabWidget->currentIndex())
+	{
+		case 0:
+			previewView->setModel(new ImportTable::CSVModel(fileEdit->text(), sqliteSeparator(), this));
+			break;
+		// TODO: more stuff
+	}
+}
+
+void ImportTableDialog::createPreview(bool)
+{
+	createPreview();
+}
+
+void ImportTableDialog::customEdit_textChanged(QString)
+{
+	if (customRadioButton->isChecked())
+		createPreview();
+}
+
+/*
+Models
+ */
+ImportTable::CSVModel::CSVModel(QString fileName, QString separator, QObject * parent, int maxRows)
+	: QAbstractTableModel(),
+	m_columns(0)
+{
+	QFile f(fileName);
+	if (!f.open(QIODevice::ReadOnly | QIODevice::Text))
+	{
+		QMessageBox::warning(qobject_cast<QWidget*>(parent), tr("Data Import"),
+							 tr("Cannot open file %1 for reading.").arg(fileName));
+		return;
+	}
+
+	QTextStream in(&f);
+	int r = 0;
+	QStringList row;
+	while (!in.atEnd())
+	{
+		row = in.readLine().split(separator);
+		if (row.count() > m_columns)
+			m_columns = row.count();
+		m_values.append(row);
+		if (++r > maxRows)
+			break;
+	}
+}
+
+int ImportTable::CSVModel::rowCount(const QModelIndex & /*parent*/) const
+{
+	return m_values.count();
+}
+
+int ImportTable::CSVModel::columnCount(const QModelIndex & /*parent*/) const
+{
+	return m_columns;
+}
+
+QVariant ImportTable::CSVModel::data(const QModelIndex & index, int role) const
+{
+	if (!index.isValid())
+		return QVariant();
+	if (role == Qt::DisplayRole || role == Qt::EditRole)
+	{
+		if (m_values.count() <= index.row())
+			return QVariant();
+		if (m_values.at(index.row()).count() <= index.column())
+			return QVariant();
+		return QVariant(m_values.at(index.row()).at(index.column()));
+	}
+	return QVariant();
+}
+
+QVariant ImportTable::CSVModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+	if (role != Qt::DisplayRole)
+         return QVariant();
+
+	if (orientation == Qt::Horizontal)
+		return QString("%1").arg(section + 1);
+
+	return QVariant();
 }
--- Sqliteman/sqliteman/importtabledialog.h
+++ Sqliteman/sqliteman/importtabledialog.h
@@ -24,10 +24,40 @@ class ImportTableDialog : public QDialog, public Ui::ImportTableDialog
 	private:
 		QObject * m_parent;
 		bool sqliteImport();
+		QString sqliteSeparator();
+
+		void sqlitePreview();
 
 	private slots:
 		void fileButton_clicked();
 		void slotAccepted();
+		void createPreview(int i = 0);
+		void createPreview(bool);
+		void customEdit_textChanged(QString);
+
+};
+
+namespace ImportTable
+{
+
+	class CSVModel : public QAbstractTableModel
+	{
+		Q_OBJECT
+
+		public:
+			CSVModel(QString fileName, QString separator, QObject * parent = 0, int maxRows = 5);
+
+			int rowCount(const QModelIndex & parent = QModelIndex()) const;
+			int columnCount(const QModelIndex & parent = QModelIndex()) const;
+
+			QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
+
+			QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+
+		private:
+			int m_columns;
+			QList<QStringList> m_values;
+	};
 
 };
 
