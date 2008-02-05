9d3fa125f94cbb28c3130b77d92b8fa36764fe8f
pvanek <petr@yarpen.cz>
2008-02-05 12:53:07 +0000

    #81: sqliteman does not build with an external qscintilla library

--- Packagers/opensuse_svn_rpm.sh
+++ Packagers/opensuse_svn_rpm.sh
@@ -4,7 +4,7 @@
 BUILDDATE=`date +'%Y%m%d'`
 
 rm ../Sqliteman/CMakeCache.txt
-svn2dist ../Sqliteman ../Sqliteman/sqliteman --no-i18n -n sqliteman -v 1.1 --log ./svn2dist.log
+./svn2dist ../Sqliteman ../Sqliteman/sqliteman --no-i18n -n sqliteman -v 1.1 --log ./svn2dist.log
 cp sqliteman-1.1.tar.gz /usr/src/packages/SOURCES
 rpmbuild --define "builddate ${BUILDDATE}" -ba sqliteman.spec
 
