006cbe970eb76f8d6d7a0d01897f10cc8ac81fd1
pvanek <petr@yarpen.cz>
2008-05-27 08:57:02 +0000

    #112: saving blob - display column size

--- Sqliteman/sqliteman/blobpreviewwidget.cpp
+++ Sqliteman/sqliteman/blobpreviewwidget.cpp
@@ -11,8 +11,9 @@ for which a new license (GPL+exception) is in place.
 
 
 BlobPreviewWidget::BlobPreviewWidget(QWidget * parent)
-	: QLabel(parent)
+	: QWidget(parent)
 {
+	setupUi(this);
 }
 
 void BlobPreviewWidget::setBlobData(QVariant data)
@@ -25,15 +26,24 @@ void BlobPreviewWidget::createPreview(QByteArray data)
 {
 	QPixmap pm;
 	pm.loadFromData(data);
+
 	if (pm.isNull())
-		setText(tr("File content cannot be displayed"));
+		m_blobPreview->setText("<qt>" + tr("File content cannot be displayed") + "</qt>");
 	else
 	{
-		if (pm.width() > width() || pm.height() > height())
-			setPixmap(pm.scaled(size(), Qt::KeepAspectRatio));
+		// HACK: "-3" constant are there to prevent recursive
+		// growing in Qt events.
+		if (pm.width() > m_blobPreview->width() - 3
+				  || pm.height() > m_blobPreview->height() - 3)
+		{
+			QSize sz(m_blobPreview->size().width()-3, m_blobPreview->size().height()-3);
+			m_blobPreview->setPixmap(pm.scaled(sz, Qt::KeepAspectRatio));
+		}
 		else
-			setPixmap(pm);
+			m_blobPreview->setPixmap(pm);
 	}
+
+	m_blobSize->setText(formatSize(m_data.size()));
 }
 
 void BlobPreviewWidget::setBlobFromFile(const QString & fileName)
@@ -50,5 +60,22 @@ void BlobPreviewWidget::setBlobFromFile(const QString & fileName)
 void BlobPreviewWidget::resizeEvent(QResizeEvent * event)
 {
 	createPreview(m_data);
-	QLabel::resizeEvent(event);
+	QWidget::resizeEvent(event);
+}
+
+QString BlobPreviewWidget::formatSize(qulonglong size/*, bool persec*/)
+{
+	QString rval;
+
+	if(size < 1024)
+		rval = QString("%L1 B").arg(size);
+	else if(size < 1024*1024)
+		rval = QString("%L1 KB").arg(size/1024);
+	else if(size < 1024*1024*1024)
+		rval = QString("%L1 MB").arg(double(size)/1024.0/1024.0, 0, 'f', 1);
+	else
+		rval = QString("%L1 GB").arg(double(size)/1024.0/1024.0/1024.0, 0, 'f', 1);
+
+// 	if(persec) rval += "/s";
+	return rval;
 }
--- Sqliteman/sqliteman/blobpreviewwidget.h
+++ Sqliteman/sqliteman/blobpreviewwidget.h
@@ -8,13 +8,15 @@ for which a new license (GPL+exception) is in place.
 #ifndef BLOBPREVIEWDIALOG_H
 #define BLOBPREVIEWDIALOG_H
 
-#include <QLabel>
+#include "ui_blobpreviewwidget.h"
+
 
 /*! \brief Brute force BLOB to Image converter.
 Methods setBlobData() and setBlobFromFile() try convert BLOBs into images
 supported by Qt4 to create a image previews.
+It displays data size for all values.
 */
-class BlobPreviewWidget : public QLabel
+class BlobPreviewWidget : public QWidget, public Ui::BlobPreviewWidget
 {
 	Q_OBJECT
 	
@@ -25,8 +27,14 @@ class BlobPreviewWidget : public QLabel
 
 	private:
 		QByteArray m_data;
+
 		void resizeEvent(QResizeEvent * event);
 		void createPreview(QByteArray data);
+
+		/*! \brief Format m_data size to the human readable form.
+		It's taken from FatRat http://fatrat.dolezel.info/. Cheers!
+		*/
+		QString formatSize(qulonglong size/*, bool persec*/);
 };
 
 #endif
